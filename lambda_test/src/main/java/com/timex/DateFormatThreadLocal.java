7edde28dee065c09ff6d4b92a1d81fb9e01c17f2
cuiwjava <cuiwjava@163.com>
2019-06-23 17:45:01 +0800

    支持重复注解 和 类型注解

--- lambda_test/src/main/java/com/timex/DateFormatThreadLocal.java
+++ lambda_test/src/main/java/com/timex/DateFormatThreadLocal.java
@@ -9,6 +9,7 @@ public class DateFormatThreadLocal {
 	
 	private static final ThreadLocal<DateFormat> df = new ThreadLocal<DateFormat>(){
 		
+		@Override
 		protected DateFormat initialValue(){
 			return new SimpleDateFormat("yyyyMMdd");
 		}
