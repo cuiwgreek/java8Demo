672e8d1cf9d65f375a15cf9266a83ed436fcba95
cuiwei34 <cuiwei34@jd.com>
2019-08-20 16:59:01 +0800

    commit

---
+++ InAction/src/main/java/com/cuiwjava/java8/Test.java
@@ -0,0 +1,22 @@
+package com.cuiwjava.java8;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+/**
+ * @Classname Test
+ * @Description TODO
+ * @Date 2019/8/20 14:05
+ * @Created by cuiwei34
+ */
+public class Test {
+	public static void main(String[] args) {
+
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd");
+		LocalDateTime now = LocalDateTime.now();
+		String nowText = now.format(df);
+		System.out.println("nowText=" + nowText);
+//		System.out.println(localDate);
+	}
+}
