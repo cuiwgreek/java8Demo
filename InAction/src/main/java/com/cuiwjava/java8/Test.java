b5edcf412613205a84b09755f5d826506463f578
cuiwei34 <cuiwei34@jd.com>
2019-10-14 23:32:53 +0800

    test

--- InAction/src/main/java/com/cuiwjava/java8/Test.java
+++ InAction/src/main/java/com/cuiwjava/java8/Test.java
@@ -12,11 +12,26 @@ import java.time.format.DateTimeFormatter;
  */
 public class Test {
 	public static void main(String[] args) {
+//		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd");
+//		LocalDateTime now = LocalDateTime.now();
+//		String nowText = now.format(df);
+//		System.out.println("nowText=" + nowText);
+//		String str = "868382034023468-1c151fee8a48,868382034023468-1C151FEE8A48";
+//		String[] devs = str.split(",");
+//
+//		for (int i = 0; i < devs.length ; i++) {
+//			System.out.println(devs[i].contains("1"));
+//		}
+//
+//		System.out.println("nowText=" + nowText);
 
-		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd");
-		LocalDateTime now = LocalDateTime.now();
-		String nowText = now.format(df);
-		System.out.println("nowText=" + nowText);
-		System.out.println("nowText=" + nowText);
+		String result = "1571062785";
+		Integer hour = 72;
+		Long time = Long.parseLong(result);
+		Long rightTime = System.currentTimeMillis() - hour * 60 * 60 * 1000;
+		System.out.println(" time is "+time + " rightTime is "+rightTime + " ====  ");
+		if (time * 1000 > rightTime) {
+
+		}
 	}
 }
